784e5884a88651dd2faa1d945d0639bfd381504e
luluci <lunacied@gmail.com>
2021-07-04 20:01:14 +0900

    add: workspace info move to class.

--- server/src/diagnoser.ts
+++ server/src/diagnoser.ts
@@ -4,13 +4,22 @@ import {
 	WorkspaceFolder,
 	WorkspaceFoldersChangeEvent,
 } from "vscode-languageserver";
+import { URI } from 'vscode-uri';
+
+export class WorkspaceInfo {
+	public rootPath: URI;
+
+	constructor(wsPath: string) {
+		this.rootPath = URI.parse(wsPath);
+	}
+}
 
 
 export class Diagnoser {
-	private _wsInfo: Map<string, WorkspaceFolder>;
+	private _wsInfo: Map<string, WorkspaceInfo>;
 	
 	constructor() {
-		this._wsInfo = new Map<string, WorkspaceFolder>();
+		this._wsInfo = new Map<string, WorkspaceInfo>();
 	}
 
 	public getWSListStr(): string {
@@ -18,31 +27,34 @@ export class Diagnoser {
 		let first = true;
 		for (const ws of this._wsInfo) {
 			if (first) {
-				result += ws[0];
+				result += ws[1].rootPath.fsPath;
 				first = false;
 			} else {
-				result += `, ${ws[0]}`;
+				result += `, ${ws[1].rootPath.fsPath}`;
 			}
 		}
 		return result;
 	}
 
 	public initWorkspaces(wss: WorkspaceFolder[]) {
-		for (const ws of wss) {
-			this._wsInfo.set(ws.name, ws);
-		}
+		this._addWorkspaces(wss);
 	}
 
 	public changeWorkspaces(event: WorkspaceFoldersChangeEvent) {
-		for (const ws of event.removed) {
-			const item = this._wsInfo.get(ws.name);
-			if (item) {
-				this._wsInfo.delete(ws.name);
-			}
-			this._wsInfo.set(ws.name, ws);
-		}
-		for (const ws of event.added) {
-			this._wsInfo.set(ws.name, ws);
+		this._deleteWorkspaces(event.removed);
+		this._addWorkspaces(event.added);
+	}
+
+	private _deleteWorkspaces(wss: WorkspaceFolder[]) {
+		for (const ws of wss) {
+			this._wsInfo.delete((ws.uri));
 		}
 	}
+
+	private _addWorkspaces(wss: WorkspaceFolder[]) {
+		for (const ws of wss) {
+			this._wsInfo.set(ws.uri, new WorkspaceInfo(ws.uri));
+		}
+	}
+
 }
